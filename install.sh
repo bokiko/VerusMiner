600a6a2ee4c63fbd69c787e657cdeb70bdcae1b4
bokiko <124489102+bokiko@users.noreply.github.com>
2023-09-03 12:31:39 +0300

    Update install.sh
    Error Handling
    Added set -e at the beginning of the script to stop execution if any command fails.
    
    Platform Checks
    Include checks to ensure the script is running on an Ubuntu system with ARM64 architecture.
    
    Sudo Access
    Check if the user has sudo access before running sudo commands.
    
    User Input
    Prompt the user for the worker name and automatically replace it in the config.json file.
    
    Package Validation
    Check if required packages like jq are installed before proceeding.
    
    Use Variables for URLs
    Store URLs in variables for easier maintenance.

--- install.sh
+++ install.sh
@@ -1,12 +1,42 @@
 #!/bin/sh
+
+# Stop the script if any command fails
+set -e
+
+# Check for Ubuntu and ARM64 architecture
+if [[ "$(uname -a)" != *Ubuntu* ]] || [[ "$(uname -m)" != *aarch64* ]]; then
+  echo "This script is intended for Ubuntu on ARM64 architecture."
+  exit 1
+fi
+
+# Check for sudo access
+if [ "$(id -u)" != "0" ]; then
+  echo "Please run as root or with sudo."
+  exit 1
+fi
+
+# Update and install packages
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget
-wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+
+# Validate that jq is installed
+if ! command -v jq &> /dev/null; then
+  echo "jq is not installed. Installing now..."
+  sudo apt-get install -y jq
+fi
+
+# Download and install libssl
+LIBSSL_URL="http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb"
+wget $LIBSSL_URL
 sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+
+# Setup ccminer
 mkdir ~/ccminer
 cd ~/ccminer
+
+# Download latest release from GitHub
 GITHUB_RELEASE_JSON=$(curl --silent "https://api.github.com/repos/Oink70/Android-Mining/releases?per_page=1" | jq -c '[.[] | del (.body)]')
 GITHUB_DOWNLOAD_URL=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets | .[] | .browser_download_url")
 GITHUB_DOWNLOAD_NAME=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets | .[] | .name")
@@ -17,18 +47,16 @@ wget ${GITHUB_DOWNLOAD_URL} -O ~/ccminer/ccminer
 wget https://raw.githubusercontent.com/TheRetroMike/VerusCliMining/main/config.json -O ~/ccminer/config.json
 chmod +x ~/ccminer/ccminer
 
+# Prompt for worker name
+read -p "Enter your worker name: " worker_name
+sed -i "s/DEFAULT_WORKER_NAME/$worker_name/" ~/ccminer/config.json
+
+# Create start.sh
 cat << EOF > ~/ccminer/start.sh
 #!/bin/sh
 ~/ccminer/ccminer -c ~/ccminer/config.json
 EOF
 chmod +x start.sh
 
-echo "setup nearly complete."
-echo "Edit the config with \"nano ~/ccminer/config.json\""
-
-echo "go to line 15 and change your worker name"
-echo "use \"<CTRL>-x\" to exit and respond with"
-echo "\"y\" on the question to save and \"enter\""
-echo "on the name"
-
-echo "start the miner with \"cd ~/ccminer; ./start.sh\"."
+echo "Setup is complete."
+echo "Start the miner with 'cd ~/ccminer; ./start.sh'."
